131d0283c9dd175cad5ff4a7a50a6885648bed20
rogelio.crisostomo <rog.cris01@outlook.com>
2024-09-20 21:32:34 -0600

    Practica-1

---
+++ Practica-2/Problema.java
@@ -0,0 +1,7 @@
+ public class Problema {
+    public static void main (String[] args) {
+        double base = 5;
+        double exponente = Math.pow(base, 3);
+        System.out.println("La base 5 con exponente 3 es:" + exponente);
+    }
+ }
